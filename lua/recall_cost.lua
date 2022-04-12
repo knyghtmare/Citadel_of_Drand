70b9a92c43009a13d1a0d8fffcbe4cf9ba1c41f0
Tahsin Jahin Khalid <tahsinjahinkhalid@gmail.com>
2022-04-12 07:23:34 +0600

    Update recall_cost.lua

--- lua/recall_cost.lua
+++ lua/recall_cost.lua
@@ -1,3 +1,35 @@
+-- original code credits to fluffbeast
+-- modified by Lord-Knightmare to accomodate lvl 1 and lvl 2 units
+
+local cost_lvl_zero = 15
+local cost_lvl_one = 25
+local cost_lvl_two = 35
+local cost_lvl_three = 45
+local cost_lvl_four = 55
+local cost_lvl_five = 70
+
 for _, unit in ipairs(wesnoth.units.find_on_recall {}) do
-    unit.recall_cost = unit.cost
+    if unit.level == 0 then
+        unit.recall_cost = cost_lvl_zero
+    end
+
+    if unit.level == 1 then
+        unit.recall_cost = cost_lvl_one
+    end
+
+    if unit.level == 2 then
+        unit.recall_cost = cost_lvl_two
+    end
+
+    if unit.level == 3 then
+        unit.recall_cost = cost_lvl_three
+    end
+
+    if unit.level == 4 then
+        unit.recall_cost = cost_lvl_four
+    end
+
+    if unit.level == 5 then
+        unit.recall_cost = cost_lvl_five
+    end
 end
